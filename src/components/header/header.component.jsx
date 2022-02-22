ff5f561c97891bc54c3625d36bb8c7de4a2d61f7
georgebrianb <george_brianb@yahoo.com>
2022-02-22 12:17:46 +0200

    integrated firebase into app, created signin page with signin component and signin with google

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,22 +1,33 @@
 import React from "react";
-import {Link} from 'react-router-dom';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { Link } from "react-router-dom";
 
-import './header.styles.scss';
+import { auth } from "../../firebase/firebase.utils";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import "./header.styles.scss";
 
-
-
-const Header = () => (
-  <div className="header">
-    <Link to="/" className="logo-container">
-      <Logo className="logo"/>
+const Header = ({ currentUser }) => (
+  <div className='header'>
+    <Link to='/' className='logo-container'>
+      <Logo className='logo' />
     </Link>
-    <div className="options"> 
-    <Link to='/shop' className="option">SHOP</Link>
-    <Link to='/contact' className="option">CONTACT</Link>
-    <Link to='/signin' className="option">SIGN IN</Link>
+    <div className='options'>
+      <Link to='/shop' className='option'>
+        SHOP
+      </Link>
+      <Link to='/contact' className='option'>
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className='option' onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link to='/signin' className='option'>
+          SIGN IN
+        </Link>
+      )}
     </div>
   </div>
-)
+);
 
 export default Header;
